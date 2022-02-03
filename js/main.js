8278a8b2e04d7a837a7ccca32052b17c473b10a5
safatas <302710276@student.rocmondriaan.nl>
2022-02-03 13:05:38 +0100

    .

--- js/main.js
+++ js/main.js
@@ -1,16 +1,11 @@
 console.log ('file loaded');
 
-let outsideTemp = prompt('Vul hier wat');
+let outSidetemp = prompt('Vul hier wat');
 
- function outSideTemp () {
-     if (outsideTemp <= 9 ) {
-         alert('Het is koud');
-     } else if (outsideTemp < 16 ) {
-         document.body.innerHTML +=  `Dat het niet warm is, en dat deze beter een jas aan kan doen.`;
-     } else if (outsideTemp  < 24 ) {
-         document.body.innerHTML += ` dat het niet koud en niet heel warm is.`;
-     } else if (outsideTemp > 24 ) {
-         document.body.innerHTML += `dat de gebruiker geen jas aan hoeft vandaag.`;
-     }
- }
+function outsidetemp(temprature) {
+  if  (outSidetemp  <= 9 ) {
+       alert('Het is nu koud');
+  }  
+}
 
+outSidetemp(temprature);
