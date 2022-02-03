69a3dc1c769e7a5cf96a1c7612c4ee60fcfb889a
safatas <302710276@student.rocmondriaan.nl>
2022-02-03 10:55:29 +0100

    opdraht maken

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 console.log ('file loaded');
 
-function sayHello() {
-   console.log('Hello');
-}
-sayHello();
-sayHello();
-sayHello();
+let outsideTemp = prompt('Vul hier wat');
 
-function isOldEnough() {
-    let oldEnough = false;
-    if (playerAge >= 18) {
-        oldEnough = true;
-    }
-}
+ function outSideTemp () {
+     if (outsideTemp <= 9 ) {
+         alert('Het is koud');
+     } else if (outsideTemp < 16 ) {
+         document.body.innerHTML +=  `Dat het niet warm is, en dat deze beter een jas aan kan doen.`;
+     } else if (outsideTemp  < 24 ) {
+         document.body.innerHTML += ` dat het niet koud en niet heel warm is.`;
+     } else if (outsideTemp > 24 ) {
+         document.body.innerHTML += `dat de gebruiker geen jas aan hoeft vandaag.`;
+     }
+ }
+
