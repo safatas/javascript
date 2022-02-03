a1c81084f76aae6f46faee11bdd7df6ee236bf0e
safatas <302710276@student.rocmondriaan.nl>
2022-02-03 09:53:16 +0100

    sfds

---
+++ js/main.js
@@ -0,0 +1,15 @@
+console.log ('file loaded');
+
+function sayHello() {
+   console.log('Hello');
+}
+sayHello();
+sayHello();
+sayHello();
+
+function isOldEnough() {
+    let oldEnough = false;
+    if (playerAge >= 18) {
+        oldEnough = true;
+    }
+}
