6ff8cfb310b333954af85ceabe75c51d0b3c8c5d
safatas <302710276@student.rocmondriaan.nl>
2022-03-17 11:30:10 +0100

    sad

--- js/toets.js
+++ js/toets.js
@@ -1,40 +1,54 @@
 
-const cars = [
-    {
-        merk: 'ford',
-        type: 'a1'
-    },
-    {
-        merk: 'volkswagen',
-        type: 'golf 7'
-    },
-    {
-        merk: 'mercedes',
-        type: 's klasse'
-    }
-    ];
+// const cars = [
+//     {
+//         merk: 'ford',
+//         type: 'a1'
+//     },
+//     {
+//         merk: 'volkswagen',
+//         type: 'golf 7'
+//     },
+//     {
+//         merk: 'mercedes',
+//         type: 's klasse'
+//     }
+//     ];
 
   
 
-console.log(cars[1]);
+// console.log(cars[1]);
 
-let i = 0;
+// let i = 0;
 
-while (i < cars.length) {
-    console.log(cars[i].merk);
-    i++;
-}
-
-for (i = 0; i < cars.length; i++){
-    console.log(cars[i]);
-}
-
-
-
-
-// let number = 1;
-// //Dit blijft doorgaan zolang number kleiner is dan 10;
-// while (number < 10) {
-//     console.log(number);
-//     number++; //Verhoog number met 1;
+// while (i < cars.length) {
+//     console.log(cars[i].merk);
+//     i++;
 // }
+
+// for (i = 0; i < cars.length; i++){
+//     console.log(cars[i]);
+// }
+
+
+// let userPoint = prompt('vul hier een cijfer in');
+ 
+// // let i = 0;
+
+// // while (i < userPoint) {
+// //     console.log(i);
+// //     i++
+// // }
+
+//  for (i = 0; i < userPoint; i++ ) {
+//      console.log(i );
+//  }
+
+ let i =0;
+ let foot =30.49;
+ 
+ while (i <= 10) {
+     i++;
+     console.log(i * 30,49);
+ }
+
+
---
+++ js/toets2.js
@@ -0,0 +1,14 @@
+const myName = 'Safa Tas';
+console.log(myName);
+
+let tempraTure = 15;
+alert(`de tempratuur van vandaag is ${tempraTure}`);
+tempraTure = tempraTure + 5 ;
+console.log(tempraTure);
+
+const result = confirm ('Wilt u cookies gaan accepteren');
+if (result) {
+    alert('je heb geaccepteerd');
+} else if (result) {
+    alert('je heb niet geaccepteerd');
+}
