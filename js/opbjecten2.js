4fde6803004787ee7f0e0a0ceaaefc9f7e7ff10c
safatas <302710276@student.rocmondriaan.nl>
2022-06-02 09:58:20 +0200

    quiz gemaakt

--- js/opbjecten2.js
+++ js/opbjecten2.js
@@ -1,48 +1,95 @@
-console.log('objecten2.js');
-
-const myArray = [
-    {
-        name : 'Safa Tas',
-        street: 'schalkburgerstraat 152',
-        city : 'DenHaag',
-    },
-    {
-        name : 'ahmet TAS',
-        street: 'schalkburgerstraat 152',
-        city : 'DenHaag',
-    },
-    {
-        name : 'ALI Tas',
-        street: 'schalkburgerstraat 152',
-        city : 'DenHaag',
-    },
-    {
-        name : 'Zeliha  Tas',
-        street: 'schalkburgerstraat 152',
-        city : 'DenHaag',
-    },
-];
-const myText = document.querySelector('.div');
 
 const myButton = document.querySelector('.submit');
-myButton.addEventListener('clik', mySafa);
+myButton.addEventListener('click',myVragen);
 
+const questions = [
+    {
+        "question": "Wat is mijn naam",
+        "optionA": "Safa Tas",
+        "optionB": "Berdan yilmaz",
+        "optionC": "Senna oudshoorn",
+        "optionD": "Bronco miete",
+        "answer": "Safa Tas"
+    },
 
+    {
+        "question": "Waar woon ik ?",
+        "optionA": "DenHaag",
+        "optionB": "Rotterdam",
+        "optionC": "China",
+        "optionD": "Turkije",
+        "answer": "DenHaag"
+    },
 
+    {
+        "question": "Welke school ga ik ",
+        "optionA": "Roc mondriaan",
+        "optionB": "Mbo rijnland",
+        "optionC": "Rijswijks lyceum",
+        "optionD": "Safa college",
+        "answer": "Roc mondriaan"
+    },
 
-function mySafa() {
-    for (let i = 0 ; i < myArray.length ; i++) {
-        let items = myArray[i]
+    {
+        "question": "Welke auto heb ik",
+        "optionA": "Golf 7",
+        "optionB": "Mercedes",
+        "optionC": "Porsche",
+        "optionD": "skoda",
+        "answer": "Golf 7"
+    },
+
+];
+
+function myVragen() {
+    const container = document.querySelector(".container");
+
+    let html = '<div class="row">';
+
+    for (let i = 0; i < questions.length; i++) {
+        const question = questions[i];
     
-        document.body.innerHTML += `
-        <div class="card" style="width: 18rem;">
-      <div class="card-body">
-        <h5 class="card-title">${items.name}</h5>
-        <p class="card-text">${items.street}</p>
-        <p class="card-text">${items.city}</p>
-      </div>
-    </div>
-    `
+        html += `
+            <div class="question">
+                <h4 class="title">Question ${i} ${question.question}</h4>
+                <p class="option">${question.optionA}</p>
+                <p class="option">${question.optionB}</p>
+                <p class="option">${question.optionC}</p>
+                <p class="option">${question.optionD}</p>
+            </div>
+            <br>
+        `;
     }
-};
+
+    html += '</div>';
+
+    container.innerHTML += html;
+
+    const quizQuestions = document.querySelectorAll('.question');
+
+    for(let i = 0; i < quizQuestions.length; i++) {
+        
+        const quizQuestion = quizQuestions[i];
+
+        const quizQuestionOptions = quizQuestion.querySelectorAll('.option');
+
+        for(let a = 0; a < quizQuestionOptions.length; a++) {
+            const quizQuestionOption = quizQuestionOptions[a];
+            
+            quizQuestionOption.addEventListener('click', function() {
+                checkAnswer(i, quizQuestionOption.textContent);
+            });        
+        }
+    }
+
+    function checkAnswer(i, givenAnswer) {
+        let currentQuizQuestion = questions[i];
+        
+        if(currentQuizQuestion.answer === givenAnswer) {
+            alert("Correct!")
+        } else {
+            alert("Wrong!")
+        }
+    }
+}
 
--- js/quiz.js
+++ js/quiz.js
@@ -1,65 +1,94 @@
-const container = document.querySelector('.container')
-
-   
-
 
 const quizOpdracht = [
     {
-        quistion : 'Wat is mijn naam',
+        questions : 'Wat is mijn nnaam',
         answerWrong1 : 'MAthijs',
         answerWrong2: 'Eren',
         answerWrong3 : 'Daryan',
         answerGood :   'Safa',
     },
     {
-        quistion : 'Welke school ga ik',
+        questions : 'Welke school ga ik',
         answerWrong1 : 'ROcmondriaan',
         answerWrong2 : 'Mborijnlanf',
         answerWrong3 : 'Grafisch lyceum',
         answerGood :   'Rocmondriaan',
     },
     {
-        quistion : 'WAt voor werk doe ik',
+        questions : 'WAt voor werk doe ik',
         answerWrong1 : 'Bezorger',
         answerWrong2 : 'Dokter',
         answerWrong3 : 'POlitie',
         answerGood :   'Bezorger',
     },
     {
-        quistion : 'Waar ben ik geboren',
+        questions : 'Waar ben ik geboren',
         answerWrong1 : 'Turkije',
         answerWrong2 : 'Senegal',
         answerWrong3 : 'Nederland',
         answerGood :   'Turkije',
     },
     {
-        quistion : 'Waar woon ik',
+        questions : 'Waar woon ik',
         answerWrong1 : 'DenHaag',
-        answerWron2 : 'Elazig',
+        answerWrong2 : 'Elazig',
         answerWrong3 : 'Rijswijk',
         answerGood :   'DenHAag',
     },
  
 ]
-let html = '<div class="row">';
+
+let html = '<div class="row">;'
 
 for(let i = 0; i < quizOpdracht.length; i++) {
     let items = quizOpdracht[i];
 
     document.body.innerHTML += `
     <h1 class="question">${items.quistion}</h1>
-    <h5 class="answerWrong1"><input type="radio" id="html" name="fav_language" value="HTML">
+    <h5 class="answerWrong"><input type="radio" id="html" name="fav_language" value="HTML">
     <label for="html">${items.answerWrong1}</label><br></h5>
-    <h5 class="answerWrong2"><input type="radio" id="html" name="fav_language" value="HTML">
+    <h5 class="answerWrong"><input type="radio" id="html" name="fav_language" value="HTML">
     <label for="html">${items.answerWrong2}</label><br></h5>
-    <h5 class="answerWrong3"><input type="radio" id="html" name="fav_language" value="HTML">
+    <h5 class="answerWrong"><input type="radio" id="html" name="fav_language" value="HTML">
     <label for="html">${items.answerWrong3}</label><br></h5>
 `
-}
+};
 
 html += '</div>';
 
-container.innerHTML += html;
+const quizQuestions = document.querySelectorAll('.question');
 
-const quizQuestions = 
+
+
+for(let i = 0; i < quizQuestions.length; i++) {
+
+    const quizQuestion = quizQuestions[i];
+
+    const quizQuestionOptions = quizQuestion.querySelectorAll('.answerWrong');
+    console.log(`quizQuestionOptions = ${quizQuestionOptions.length}`);
+
+    
+    for(let a = 0; a < quizQuestionOptions.length; a++) {
+        const quizQuestionOption = quizQuestionOptions[a];
+       
+
+        quizQuestionOption.addEventListener('click', function() {
+            checkAnswer(i, quizQuestionOption.textContent);
+            console.log("quizQuestionOption.textConten: " + quizQuestionOption.textConten);
+        });        
+    }
+ 
+};
+
+function checkAnswer(i, givenAnswer) {
+    let currentQuizQuestion = questions[i];
+   
+    
+    if(currentQuizQuestion.answer === givenAnswer) {
+        alert("Correct!")
+    } else {
+        alert("Wrong!")
+    }
+};
+
 
